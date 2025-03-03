336c1481912aad6684d4f23b27cfe47c3d3e157a
potapchukdmytro <traiget@gmail.com>
2025-03-03 17:16:18 +0200

    Car entity and controller

--- pd311_web_api/Program.cs
+++ pd311_web_api/Program.cs
@@ -1,9 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using pd311_web_api.DAL;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+// Add database context
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql("name=NpgsqlLocal");
+});
+
 //builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
 
